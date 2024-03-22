16a1a2dafb63aab4637c6973cb5f68216a327a6a
mu-ji <1142260696@qq.com>
2024-03-22 16:25:25 +0800

    enable accelerometer

--- projects/nrf52840_dk/01bsp_bmx160/01bsp_bmx160.c
+++ projects/nrf52840_dk/01bsp_bmx160/01bsp_bmx160.c
@@ -23,8 +23,9 @@ Then it sends out the gyro data through uart at interval of SAMPLE_PERIOD.
 //=========================== defines =========================================
 
 #define SAMPLE_PERIOD   (32768>>4)  // @32kHz = 1s
-#define BUFFER_SIZE     0x08   //2B*3 axises value + 2B ending with '\r\n'
-
+//#define BUFFER_SIZE     0x08   //2B*3 axises value + 2B ending with '\r\n'
+//#define BUFFER_SIZE     0x0E   //2B*3 gyr + 2B*3 acc + 2B ending with '\r\n'
+#define BUFFER_SIZE     0x14   //2B*3 gyr + 2B*3 acc + 2B ending with '\r\n'
 //=========================== variables =======================================
 
 typedef struct {
@@ -80,7 +81,10 @@ int mote_main(void) {
     i2c_set_addr(BMX160_ADDR);
 
     // should be 0xd8 for bmx160
-    app_vars.who_am_i = bmx160_who_am_i();    
+    app_vars.who_am_i = bmx160_who_am_i();
+    
+    bmx160_config_wakeup();
+    //bmx160_set_mode(0x15);    
   
     // configure bmx160
 
@@ -91,7 +95,7 @@ int mote_main(void) {
     // 0x8: 100Hz
     // 0xc: 1600Hz
     // 0xd: 3200Hz
-    bmx160_gyr_config(0x0d);
+    bmx160_gyr_config(0x0c);
     // 0x8: 100Hz
     // 0xb: 800Hz
     bmx160_mag_config(0x0b);
@@ -107,7 +111,7 @@ int mote_main(void) {
     // 0x3: +/-250°/s  = 262.4LSB/°/s
     bmx160_gyr_range(0x1);
     // ToDo
-    //bmx160_mag_if();
+    bmx160_mag_if(0x01);
 
     while (1) {
 
@@ -117,6 +121,7 @@ int mote_main(void) {
         app_vars.uartSendNow = 0;
 
         bmx160_read_9dof_data();
+        //i2c_read_bytes(BMX160_REG_ADDR_DATA, (uint8_t*)(&bmx160x_var.bmx160x_data), sizeof(bmx160x_data_t));
 
         i=0;
         tmp = bmx160_read_gyr_x();
@@ -130,6 +135,30 @@ int mote_main(void) {
         tmp = bmx160_read_gyr_z();
         app_vars.uartToSend[i++] = (uint8_t)((tmp>>8) & 0x00ff);
         app_vars.uartToSend[i++] = (uint8_t)((tmp>>0) & 0x00ff);
+
+        tmp = bmx160_read_acc_x();
+        app_vars.uartToSend[i++] = (uint8_t)((tmp>>8) & 0x00ff);
+        app_vars.uartToSend[i++] = (uint8_t)((tmp>>0) & 0x00ff);
+
+        tmp = bmx160_read_acc_y();
+        app_vars.uartToSend[i++] = (uint8_t)((tmp>>8) & 0x00ff);
+        app_vars.uartToSend[i++] = (uint8_t)((tmp>>0) & 0x00ff);
+
+        tmp = bmx160_read_acc_z();
+        app_vars.uartToSend[i++] = (uint8_t)((tmp>>8) & 0x00ff);
+        app_vars.uartToSend[i++] = (uint8_t)((tmp>>0) & 0x00ff);
+
+        //tmp = bmx160_read_mag_x();
+        //app_vars.uartToSend[i++] = (uint8_t)((tmp>>8) & 0x00ff);
+        //app_vars.uartToSend[i++] = (uint8_t)((tmp>>0) & 0x00ff);
+
+        //tmp = bmx160_read_mag_y();
+        //app_vars.uartToSend[i++] = (uint8_t)((tmp>>8) & 0x00ff);
+        //app_vars.uartToSend[i++] = (uint8_t)((tmp>>0) & 0x00ff);
+
+        //tmp = bmx160_read_mag_z();
+        //app_vars.uartToSend[i++] = (uint8_t)((tmp>>8) & 0x00ff);
+        //app_vars.uartToSend[i++] = (uint8_t)((tmp>>0) & 0x00ff);
 
         app_vars.uartToSend[i++] = '\r';
         app_vars.uartToSend[i++] = '\n';
@@ -156,7 +185,8 @@ void cb_compare(void) {
 void cb_uartTxDone(void) {
 
     app_vars.uart_lastTxByteIndex++;
-    if (app_vars.uart_lastTxByteIndex<sizeof(app_vars.uartToSend)) {
+    //if (app_vars.uart_lastTxByteIndex<sizeof(app_vars.uartToSend)) {
+    if (app_vars.uart_lastTxByteIndex<14) {
         uart_writeByte(app_vars.uartToSend[app_vars.uart_lastTxByteIndex]);
     } else {
         app_vars.uartDone = 1;
